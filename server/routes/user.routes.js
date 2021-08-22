1ba643e84e30a1d996ba9818c6d8ac18001e65d4
Stefano <piero.quiroz93@gmail.com>
2021-08-21 22:14:23 -0500

    User routes modified

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,22 +1,22 @@
-const {findUser, findSingleUser, createUser, updateUser, deleteUser, login} = require("../controllers/user.controller");
-//const {authenticate} = require("../config/jwt.config");
+const {findUser, findSingleUser, createUser, updateUser, deleteUser, login, logOut} = require("../controllers/user.controller");
+const {authenticate} = require("../config/jwt.config");
 const express = require("express");
 const router = express();
 
-/* 
+
 router.get("/users", authenticate, findUser);
 router.get("/user/:id", authenticate, findSingleUser);
-router.post("/user/new", authenticate, createUser);
-router.post("/login", authenticate, login);
-router.put("/users/update/:id", authenticate, updateUser);
-router.delete("/users/delete/:id", authenticate, deleteUser);
-*/
+router.post("/user/new", createUser);
+router.put("/user/update/:id", authenticate, updateUser);
+router.delete("/user/delete/:id", authenticate, deleteUser);
 
-router.get("/users", findUser);
+router.post("/login", login);
+router.get("/logout", authenticate, logOut);
+module.exports = router; 
+
+/* router.get("/users", findUser);
 router.get("/user/:id", findSingleUser);
 router.post("/user/new", createUser);
-router.post("/login", login);
 router.put("/users/update/:id", updateUser);
-router.delete("/users/delete/:id", deleteUser);
+router.delete("/users/delete/:id", deleteUser); */
 
-module.exports = router; 
