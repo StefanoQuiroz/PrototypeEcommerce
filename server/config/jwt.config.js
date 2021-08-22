7c2a49db1a2dcbdf13cf6d9a8b4d2a3d0af77df5
Stefano <piero.quiroz93@gmail.com>
2021-08-21 22:12:58 -0500

    JWT modified

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -3,7 +3,7 @@ const secretKey = process.env.SECRET_KEY;
 const authenticate = (req, res, next) => {
     jwt.verify(req.cookies.usertoken, secretKey, (error, payload) => {
         if(error){
-            res.status(400).json({verified: false});
+            res.status(401).json({verified:false})
         } else {
             next();
         }
