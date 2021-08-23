071411ff3c27d40f72b03e6b32ba77844678f512
Stefano <piero.quiroz93@gmail.com>
2021-08-23 01:07:51 -0500

    Modified authenticationAdmin

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -6,7 +6,7 @@ const User = require("../models/user.models");
 const authenticate = (req, res, next) => {
     jwt.verify(req.cookies.usertoken, secretKey, (error, payload) => {
         if(error){
-            res.status(401).json({verified:false})
+            res.status(401).json({verified:false, message: "Acesso denegado"})
         } else {
             next();
         }
@@ -15,10 +15,23 @@ const authenticate = (req, res, next) => {
 
 //Autenticación del admin
 const authenticateAdmin = (req, res, next) => {
-    User.findOne({_id: req.params.id})
+   /*  try{
+        const user = await User.findOne({
+            _id: req.user.id
+        })
+        console.log(user.data)
+        if(user.data.rol === 0)
+        return res.status(400).json({message: "Acceso denegado al Administrador"})
+         
+        next();
+    } catch(error){
+        return res.status(500).json({message: "Algo salió mal de authenticate"})
+    } */
+    User.findById({_id: req.body.id})
         .then( result => {
+            console.log("resultado", result)
             if(result.rol === 0){
-                res.json({message: "Acceso al administrador denegado"})
+                res.json({message: "Acceso denegado al Administrador"})
             } else{
                 next();
             }
