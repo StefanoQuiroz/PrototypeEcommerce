f723e5bfac7a52d4bd4d3b84e377d74db3ec5f00
Stefano <piero.quiroz93@gmail.com>
2021-08-23 01:06:09 -0500

    Added routes for category

--- server/routes/category.routes.js
+++ server/routes/category.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express();
+const {authenticate, authenticateAdmin} = require('../config/jwt.config');
 
-const {findCategory} = require("../controllers/category.controllers");
+const {findCategory, createCategory} = require("../controllers/category.controllers");
 
 router.get(`/category`, findCategory);
+router.post(`/category/create`, authenticate, authenticateAdmin, createCategory);
 
 module.exports = router;
