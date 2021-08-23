856f2bec9c7963411607d53ce8c715ea624972cd
Stefano <piero.quiroz93@gmail.com>
2021-08-22 22:32:55 -0500

    Authentication for Admin

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -1,5 +1,8 @@
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.SECRET_KEY;
+const User = require("../models/user.models");
+
+//Autenticación comun del usuario
 const authenticate = (req, res, next) => {
     jwt.verify(req.cookies.usertoken, secretKey, (error, payload) => {
         if(error){
@@ -10,4 +13,17 @@ const authenticate = (req, res, next) => {
     })
 }
 
-module.exports = {secretKey, authenticate};
+//Autenticación del admin
+const authenticateAdmin = (req, res, next) => {
+    User.findOne({_id: req.params.id})
+        .then( result => {
+            if(result.rol === 0){
+                res.json({message: "Acceso al administrador denegado"})
+            } else{
+                next();
+            }
+        })
+        .catch(error => res.json({error: error, message: "Algo salió mal"}))
+}
+
+module.exports = {secretKey, authenticate, authenticateAdmin};
